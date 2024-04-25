72e7501b80110b1f1843cc9b75dc3f3722669a24
Eneko Lazaro <70603123+elazaroo@users.noreply.github.com>
2024-04-25 11:17:31 +0200

    Added the explanation with comments

--- testSQL.java
+++ testSQL.java
@@ -2,19 +2,27 @@ import java.util.Arrays;
 
 public class testSQL {
     public static void main(String[] args) {
-        conMySQL.selectQuery("SELECT * FROM PRUEBATABLA WHERE APELLIDO ='LAZARO';");
+        // Select query and show the result
+        conMySQL.selectQuery("SELECT * FROM empleados;");
         System.out.println(Arrays.toString(conMySQL.getResult()));
 
-        conMySQL.updateQuery("UPDATE PRUEBATABLA SET NOMBRE='PEPE' WHERE ID=2;");
-        conMySQL.selectQuery("SELECT * FROM PRUEBATABLA WHERE ID=2;");
+        // Create the result array and add the data from the previous select query and show the array named result
+        String [] result = conMySQL.getResult();
+        System.out.println(Arrays.toString(result));
+
+        // Update query and show the result
+        conMySQL.updateQuery("UPDATE empleados SET NOMBRE='PEPE' WHERE ID_EMPLEADO=2;");
+        conMySQL.selectQuery("SELECT * FROM empleados WHERE ID_EMPLEADO=2;");
         System.out.println(Arrays.toString(conMySQL.getResult()));
 
-        conMySQL.insertQuery("INSERT INTO PRUEBATABLA VALUES (3,'Asier','Perez','asier@gmail.com');");
-        conMySQL.selectQuery("SELECT * FROM PRUEBATABLA;");
+        // Insert query and show the result
+        conMySQL.insertQuery("INSERT INTO empleados(nombre,apellidos,telefono,correo,direccion,pass) VALUES ('Asier','Perez',684662812,'asier@gmail.com','Direccion1','Contra1');");
+        conMySQL.selectQuery("SELECT * FROM empleados;");
         System.out.println(Arrays.toString(conMySQL.getResult()));
 
-        conMySQL.deleteQuery("DELETE FROM PRUEBATABLA WHERE ID=2;");
-        conMySQL.selectQuery("SELECT * FROM PRUEBATABLA;");
+        // Delete query and show the result
+        conMySQL.deleteQuery("DELETE FROM empleados WHERE ID_EMPLEADO=2;");
+        conMySQL.selectQuery("SELECT * FROM empleados;");
         System.out.println(Arrays.toString(conMySQL.getResult()));
     }
 }
